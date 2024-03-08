9065988e0560245790caa5eb37016fe8108e7e15
Lalataaksh S <129892213+lalataakshs@users.noreply.github.com>
2024-03-08 10:33:33 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"If you see this, it totally worked 🤌"<<endl;
+    cout<<"If not, then heres a skull 💀"<<endl;
+    
+    return 0;
+}
